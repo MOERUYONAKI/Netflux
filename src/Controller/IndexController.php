96e6364582c708277a71d0ec5221dd18bfced3c5
MOERUYONAKI <1bbordi1@gmail.com>
2024-11-08 14:30:20 +0100

    signin

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,48 +3,95 @@
 namespace App\Controller;
 
 use App\Entity\Movie;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'index')]
     public function index(
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        Session $session
     ): Response
     {
-        $movies = $entityManager->getRepository(Movie::class)->findAll();
+        if($session->has('user'))
+        {
+            $movies = $entityManager->getRepository(Movie::class)->findAll();
+            $user = $entityManager->getRepository(User::class)->find($session->get('user')->getId());
+            $admin = 0;
 
-        return $this->render('Page/index.html.twig', [
-            'films' => $movies
-        ]);
+            if ($user->isAdmin()) {
+                $admin = 1;
+            }
+
+            return $this->render('Page/index.html.twig', [
+                'films' => $movies,
+                'admin' => $admin
+            ]);
+        } else {
+            return $this->redirectToRoute('signin');
+        }
     }
 
-    #[Route('/registration', name: 'registration')]
-    public function inscription(): Response
+    #[Route('/signin', name: 'signin')]
+    public function inscription(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        Session $session): Response
     {
-        return $this->render('Page/index.html.twig');
+        return $this->render('Page/signin.html.twig');
     }
 
-    #[Route('/connexion', name: 'connexion')]
-    public function connexion(): Response
+    #[Route('/signout', name: 'signout')]
+    public function connexion(
+        Session $session
+    ): Response
     {
-        return $this->render('Page/index.html.twig');
+        if($session->has('user'))
+        {
+            $session->remove('user');
+        }
+
+        return $this->redirectToRoute('signin');
+    }
+
+    #[Route('/new', name: 'add')]
+    public function new(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        Session $session
+    ): Response
+    {
+        if($session->has('user') && $session->get('user')->isAdmin())
+        {
+            return $this->render('Page/index.html.twig');
+        }
+
+        return $this->redirectToRoute('index');
     }
 
     #[Route('/watch/{id}', name: 'watch')]
     public function watch(
         EntityManagerInterface $entityManager,
+        Session $session,
         int $id
     ): Response
     {
-        $movie = $entityManager->getRepository(Movie::class)->find($id);
+        if($session->has('user'))
+        {
+            $movie = $entityManager->getRepository(Movie::class)->find($id);
 
-        return $this->render('Page/watch.html.twig', [
-            'film' => $movie
-        ]);
+            return $this->render('Page/watch.html.twig', [
+                'film' => $movie
+            ]);
+        } else {
+            return $this->redirectToRoute('signin');
+        }
+
     }
 }
