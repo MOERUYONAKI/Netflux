8cb6eb4437231512ebe870d28e89f8543338fb08
MOERUYONAKI <1bbordi1@gmail.com>
2024-11-08 11:06:34 +0100

    index

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -11,6 +11,35 @@ class IndexController extends AbstractController
     #[Route('/', name: 'index')]
     public function index(): Response
     {
+        $testFilms = [
+            [
+                "title" => "Astérix et Obélix : L'Empire Du Milieu",
+                "date" => "",
+                "image" => "/medias/L'Empire Du Milieu.jpg",
+                "movie" => ""
+            ],
+        ];
+
+        return $this->render('Page/index.html.twig', [
+            'films' => $testFilms
+        ]);
+    }
+
+    #[Route('/incription', name: 'inscription')]
+    public function inscription(): Response
+    {
+        return $this->render('Page/index.html.twig');
+    }
+
+    #[Route('/connexion', name: 'connexion')]
+    public function connexion(): Response
+    {
+        return $this->render('Page/index.html.twig');
+    }
+
+    #[Route('/watch/{id}', name: 'watch')]
+    public function watch(): Response
+    {
         return $this->render('Page/index.html.twig');
     }
 }
